96a5d7ccbe702fad7f43e960cfda5c8814557fe3
Drew Thomas <drew.thomas@comcast.net>
2018-10-03 09:03:05 -0600

    still don't understand how this works

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<vector<vector<int>>> grid;
+    /*
+    vector<int> myvector;
+    int myint;
+    cout << "enter a number" << endl;
+    cin >> myint;
+    myvector.push_back(myint);
+    cout << myvector [0] << endl << endl << endl;
+    */
+    /*
+    sollist.push_back(vector<vector<int>>());
+    sollist[0].push_back(vector<int>());
+    sollist[0][0].push_back(value);
+    */
+    for (int i = 0; i <= 9 ; i++) {
+        grid.push_back(vector<vector<int>>());
+        grid[0].push_back(vector<int>());
+        grid[0][0].push_back(1);
+        
+//        for (int j = 0; j <= 9; j++) {
+//            for (int k = 0; k <= 9; k++){
+//                grid[i][j][k];
+//            }
+//        }
+    }
+    cout << grid[0][0][0];
     return 0;
 }
