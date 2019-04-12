819e4cd9b87803098313a6231676851a3554bba9
Drew Thomas <drew.thomas@comcast.net>
2019-04-11 20:17:20 -0600

    some things work i think

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -7,17 +7,57 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <vector>
 using namespace std;
 
-//void()
+int x = 2;
+int y = 2;
+int real = x + 1;
+int total = ((x + 1) * (x + 2))/2;
+int line1total = total;
+int line2total = total;
+int line3total = total;
+int column1total = total;
+int column2total = total;
+int column3total = total;
+vector < vector < vector<int> > > tube;
+
+void func(int lineTotal){
+	if(real == 3){
+		bool slot1;
+		bool slot2;
+		bool slot3;
+		slot1 = tube[0][0][0];
+		slot2 = tube[0][0][0];
+		slot3 = tube[0][0][0];
+		if(tube[0][0][0] == 0){
+			slot1 = false;
+		}else{
+			slot1 = true;
+		}
+		if(tube[0][1][0] == 0){
+			slot2 = false;
+		}else{
+			slot2 = true;
+		}
+		if(tube[0][2][0] == 0){
+			slot3 = false;
+		}else{
+			slot3 = true;
+		}
+		for(int i=0;i<=x;i++){
+			line1total -= tube[0][i][0];
+		}
+		cout << line1total << endl;
+		cout << slot1 << endl << slot2 << endl << slot3 << endl;
+	}
+	int defacit = total - line1total;
+	cout << defacit << endl;
+}
 
 int main(int argc, const char * argv[]) {
-    /*int x = 2;
-    int y = 2;
-    int real = x + 1;
-    int total = ((x + 1) * (x + 2))/2;
     vector < vector < vector<int> > > tube;
     for(int i=0;i<=y;i++)
     {
@@ -25,71 +65,24 @@ int main(int argc, const char * argv[]) {
         for(int j=0;j<=x;j++)
         {
             tube[i].push_back(vector<int>());
-            for(int k=0;k<5;k++)
+            for(int k=0;k<1;k++)
             {
                 int a;
-//                cout << "[" << i << " , " << j << ", ";
-//                cin >> a;
-                tube[i][j].push_back(0);
-//                cout << "[" << i << ", " << j << ", " << tube[i][j][k] << "]" << endl;
+				cout << "[" << i << ", " << j << ", ";
+                cin >> a;
+                tube[i][j].push_back(a);
+				cout << "[" << i << ", " << j << ", " << tube[i][j][k] << "]" << endl;
             }
         }
     }
-    for(int i=0;i<=x;i++){
-        for(int j=0;j<=y;j++){
+    for(int i=0;i<=y;i++){
+        for(int j=0;j<=x;j++){
             cout << "|" << tube[i][j][0];
         }
         cout << "|" << endl;
     }
-    
-    if(real == 3){
-		int line1total = total;
-		bool slot1;
-		bool slot2;
-		bool slot3;
-		slot1 = tube[0][0][0];
-//		if(tube[0][0][0] == 0){
-//			slot1 = false;
-//		}else{
-//			slot1 = true;
-//		}
-//		if(tube[0][1][0] == 0){
-//			slot2 = false;
-//		}else{
-//			slot2 = true;
-//		}
-//		if(tube[0][2][0] == 0){
-//			slot3 = false;
-//		}else{
-//			slot3 = true;
-//		}
-//		for(int i=0;i<=x;i++){
-//			line1total -= tube[0][i][0];
-//		}
-		cout << line1total << endl;
-		
-    }
-//    cout << "|" << tube[0][0][0] << "|" << tube[0][1][0] << "|" << tube[0][2][0] << "|" << endl;
-//    cout << "|" << tube[1][0][0] << "|" << tube[1][1][0] << "|" << tube[1][2][0] << "|" << endl;
-//    cout << "|" << tube[2][0][0] << "|" << tube[2][1][0] << "|" << tube[2][2][0] << "|" << endl;
-//    cout << tube[0][0][0];
-//    for(int i=0;i<10;i++){
-//        for(int j=0;j<10;j++){
-//            int a;
-//            cout << "[" << i << " , " << j << ", ";
-//            cin >> a;
-//            tube[i][j].push_back(a);
-//        }
-//    }*/
-	
-	struct slot {
-		int num;
-		bool nonzero = true;
-	};
-	
-	vector<vector<slot>> vector;
-	vector.
-	
+	func(3);
+
 	
     return 0;
 }
