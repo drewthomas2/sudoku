6b0eca58c0487b2b27b3ed1ba1f57d772fecebad
Drew Thomas <drew.thomas@comcast.net>
2019-04-26 06:37:07 -0600

    made it better

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -5,59 +5,29 @@
 //  Copyright © 2018 Drew Thomas. All rights reserved.
 //
 
-#define UNASSIGNED 0
-
-#include <iostream>
-#include <fstream>
-#include <iomanip>
+#include <stdio.h>
 #include <vector>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-int N = 9;
-vector < vector < vector<int> > > grid;
+// UNASSIGNED is used for empty cells in sudoku grid
+#define UNASSIGNED 0
 
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
-bool isSafe(int grid[N][N], int row, int col, int num);
+// N is used for the size of Sudoku grid. Size will be NxN
+#define N 9
 
-bool SolveSudoku(int grid[N][N])
-{
-	int row, col;
-	
-	// If there is no unassigned location,
-	// we are done
-	if (!FindUnassignedLocation(grid, row, col))
-		return true; // success!
-	
-	// consider digits 1 to 9
-	for (int num = 1; num <= 9; num++)
-	{
-		// if looks promising
-		if (isSafe(grid, row, col, num))
-		{
-			// make tentative assignment
-			grid[row][col] = num;
-			
-			// return, if success, yay!
-			if (SolveSudoku(grid))
-				return true;
-			
-			// failure, unmake & try again
-			grid[row][col] = UNASSIGNED;
-		}
-	}
-	return false; // this triggers backtracking
-}
+// This function finds an entry in grid that is still unassigned
+//bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
 
-bool FindUnassignedLocation(int grid[N][N],
-							int &row, int &col)
-{
-	for (row = 0; row < N; row++)
-		for (col = 0; col < N; col++)
-			if (grid[row][col] == UNASSIGNED)
-				return true;
-	return false;
-}
+// Checks whether it will be legal to assign num to the given row, col
+//bool isSafe(int grid[N][N], int row, int col, int num);
 
+
+bool SolveSudoku(int grid[N][N]);
+
+/* Returns a boolean which indicates whether an assigned entry
+ in the specified row matches the given number. */
 bool UsedInRow(int grid[N][N], int row, int num)
 {
 	for (int col = 0; col < N; col++)
@@ -66,6 +36,8 @@ bool UsedInRow(int grid[N][N], int row, int num)
 	return false;
 }
 
+/* Returns a boolean which indicates whether an assigned entry
+ in the specified column matches the given number. */
 bool UsedInCol(int grid[N][N], int col, int num)
 {
 	for (int row = 0; row < N; row++)
@@ -74,44 +46,116 @@ bool UsedInCol(int grid[N][N], int col, int num)
 	return false;
 }
 
+/* Returns a boolean which indicates whether an assigned entry
+ within the specified 3x3 box matches the given number. */
 bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)
 {
 	for (int row = 0; row < 3; row++)
 		for (int col = 0; col < 3; col++)
-			if (grid[row + boxStartRow] [col + boxStartCol] == num)
+			if (grid[row+boxStartRow][col+boxStartCol] == num)
 				return true;
 	return false;
 }
 
-bool isSafe(int grid[N][N], int row, int col, int num)
+/* Searches the grid to find an entry that is still unassigned. If
+ found, the reference parameters row, col will be set the location
+ that is unassigned, and true is returned. If no unassigned entries
+ remain, false is returned. */
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col)
 {
-	/* Check if 'num' is not already placed in
-	 current row, current column and current 3x3 box */
-	return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row % 3 , col - col % 3, num) && grid[row][col] == UNASSIGNED;
+	for (row = 0; row < N; row++)
+		for (col = 0; col < N; col++)
+			if (grid[row][col] == UNASSIGNED)
+				return true;
+	return false;
 }
 
-int main(int argc, const char * argv[]) {
-	vector<vector<int>> grid;
-	for(int i=0;i<=N;i++)
+/* Returns a boolean which indicates whether it will be legal to assign
+ num to the given row,col location. */
+bool isSafe(int grid[N][N], int row, int col, int num)
+{
+	/* Check if 'num' is not already placed in current row, current column and current 3x3 box */
+	return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row%3 , col - col%3, num)&& grid[row][col]==UNASSIGNED;
+}
+
+/* Takes a partially filled-in grid and attempts to assign values to
+ all unassigned locations in such a way to meet the requirements
+ for Sudoku solution (non-duplication across rows, columns, and boxes) */
+bool SolveSudoku(int grid[N][N])
+{
+	int row, col;
+	
+	// If there is no unassigned location, we are done
+	if (!FindUnassignedLocation(grid, row, col)){
+		return true; // success!
+	}
+	
+	
+	// consider digits 1 to 9
+	for (int num = 1; num <= 9; num++)
 	{
-		grid.push_back(vector<int>(7));
-		for(int j=0;j<=N;j++)
+		// if looks promising
+		if (isSafe(grid, row, col, num)){
+			// make tentative assignment
+			grid[row][col] = num;
+			
+			// return, if success, yay!
+			if (SolveSudoku(grid)){
+				return true;
+			}
+			
+			// failure, unmake & try again
+			grid[row][col] = UNASSIGNED;
+		}
+	}
+	return false; // this triggers backtracking
+}
+
+/* A utility function to print grid */
+void printGrid(int grid[N][N])
+{
+	for (int row = 0; row < N; row++)
+	{
+		for (int col = 0; col < N; col++)
+			cout << "|" << grid[row][col];
+	}
+	cout << "|" << endl;
+}
+
+/* Driver Program to test above functions */
+int main()
+{
+	// 0 means unassigned cells
+	//    int grid[N][N] = {
+	//        {3, 0, 6, 5, 0, 8, 4, 0, 0},
+	//        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+	//        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+	//        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+	//        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+	//        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+	//        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+	//        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+	//        {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+	
+	vector<vector<int>> tube;
+	for(int i=0;i<N;i++)
+	{
+		tube.push_back(vector<int>(7));
+		for(int j=0;j<N;j++)
 		{
 			int a;
-			cout << "enter a ton of numbers" << endl;
+			cout << "enter a shit ton of numbers" << endl;
 			cin >> a;
-			grid[i][j] = a;
-			cout << "[" << i << ", " << j << ", " << grid[i][j] << "]" << endl;
+			tube[i][j] = a;
+			cout << "[" << i << ", " << j << ", " << tube[i][j] << "]" << endl;
 		}
 	}
 	
-	for(int i=0;i<=N;i++){
-		for(int j=0;j<=N;j++){
-			//cout << i << " : " << j << " > " << grid[i][j] << endl;
-			cout << "|" << grid[i][j];
-		}
-		cout << "|" << endl;
+	if (SolveSudoku(tube) == true){
+		cout << "epic victory royale"<< endl;
 	}
+	else
+		cout << "No solution exists" << endl;
 	
 	return 0;
 }
