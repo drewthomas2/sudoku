163abafe53caba9fc939ce98a557aa946c43b3ba
Drew Thomas <drew.thomas@comcast.net>
2019-05-15 18:02:40 -0600

    IT FINALLY WORKS I SWITCHED TO ARRAYS AND ITS SO MUCH EASIER AND THE PRINTGRID FUNCTION WORKS SO WELL

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -4,67 +4,23 @@
 //  Created by Drew Thomas on 8/31/18.
 //  Copyright © 2018 Drew Thomas. All rights reserved.
 //
-
-#include <stdio.h>
-#include <vector>
-#include <iomanip>
 #include <iostream>
+
 using namespace std;
 
 #define UNASSIGNED 0
 #define N 9
 
-bool SolveSudoku(vector<vector<int>> grid);
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
 
-bool UsedInRow(vector<vector<int>> grid, int row, int num){
-	for (int col = 0; col < N; col++){
-		if (grid[row][col] == num){
-			return true;
-		}
-	}
-	return false;
-}
+bool isSafe(int grid[N][N], int row, int col, int num);
 
-bool UsedInCol(vector<vector<int>> grid, int col, int num){
-	for (int row = 0; row < N; row++){
-		if (grid[row][col] == num){
-			return true;
-		}
-	}
-	return false;
-}
-
-bool UsedInBox(vector<vector<int>> grid, int boxStartRow, int boxStartCol, int num){
-	for (int row = 0; row < 3; row++){
-		for (int col = 0; col < 3; col++){
-			if (grid[row+boxStartRow][col+boxStartCol] == num){
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-bool FindUnassignedLocation(vector<vector<int>> grid, int &row, int &col){
-	for (row = 0; row < N; row++){
-		for (col = 0; col < N; col++){
-			if (grid[row][col] == UNASSIGNED){
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-bool isSafe(vector<vector<int>> grid, int row, int col, int num){
-	return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row-row%3 , col-col%3, num)&& grid[row][col]==UNASSIGNED;
-}
-
-bool SolveSudoku(vector<vector<int>> grid){
+bool SolveSudoku(int grid[N][N]){
 	int row, col;
 	if (!FindUnassignedLocation(grid, row, col)){
 		return true;
 	}
+	
 	for (int num = 1; num <= 9; num++){
 		if (isSafe(grid, row, col, num)){
 			grid[row][col] = num;
@@ -77,44 +33,135 @@ bool SolveSudoku(vector<vector<int>> grid){
 	return false;
 }
 
-void printGrid(vector<vector<int>> grid){
-	for (int row=0; row<N; row++){
-		for (int col=0; col<N; col++){
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col){
+	for (row = 0; row < N; row++){
+		for (col = 0; col < N; col++){
+			if (grid[row][col] == UNASSIGNED){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool UsedInRow(int grid[N][N], int row, int num){
+	for (int col = 0; col < N; col++){
+		if (grid[row][col] == num){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UsedInCol(int grid[N][N], int col, int num){
+	for (int row = 0; row < N; row++){
+		if (grid[row][col] == num){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num){
+	for (int row = 0; row < 3; row++){
+		for (int col = 0; col < 3; col++){
+			if (grid[row + boxStartRow][col + boxStartCol] == num){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool isSafe(int grid[N][N], int row, int col, int num){
+	return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row % 3, col - col % 3, num) && grid[row][col] == UNASSIGNED;
+}
+
+void printGrid(int grid[N][N]){
+	for (int row = 0; row < 3; row++){
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "|" << endl;
+	}
+	cout << endl;
+	for (int row = 0; row < 3; row++){
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "|" << endl;
+	}
+	cout << endl;
+	for (int row = 0; row < 3; row++){
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
+			cout << "|" << grid[row][col];
+		}
+		cout << "| ";
+		for (int col = 0; col < 3; col++){
 			cout << "|" << grid[row][col];
 		}
 		cout << "|" << endl;
 	}
 }
 
-
 int main(){
-//  {3, 0, 6, 5, 0, 8, 4, 0, 0}
-//  {5, 2, 0, 0, 0, 0, 0, 0, 0}
-//  {0, 8, 7, 0, 0, 0, 0, 3, 1}
-//  {0, 0, 3, 0, 1, 0, 0, 8, 0}
-//  {9, 0, 0, 8, 6, 3, 0, 0, 5}
-//  {0, 5, 0, 0, 9, 0, 6, 0, 0}
-//  {1, 3, 0, 0, 0, 0, 2, 5, 0}
-//  {0, 0, 0, 0, 0, 0, 0, 7, 4}
-//  {0, 0, 5, 2, 0, 6, 3, 0, 0}
-	
-	vector<vector<int>> tube;
+	int grid[N][N] = {{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},
+		{0, 0, 0,   0, 0, 0,   0, 0, 0},};
+	//    printGrid(grid);
+	cout << "enter a couple of numbers" << endl;
 	for(int i=0;i<N;i++){
-		tube.push_back(vector<int>(7));
 		for(int j=0;j<N;j++){
 			int a;
-			cout << "enter a couple of numbers" << endl;
+			cout << "[" << (i+1) << ", " << (j+1) << "] = ";
 			cin >> a;
-			tube[i][j] = a;
-			cout << "[" << (i+1) << ", " << (j+1) << "] = " << tube[i][j] << endl;
+			grid[i][j] = a;
 		}
 	}
-	printGrid(tube);
-	/*if (SolveSudoku(tube) == true){
-		cout << "epic victory royale"<< endl;
-		printGrid(tube);
+	if (SolveSudoku(grid) == true){
+		printGrid(grid);
+		cout << "it works" << endl;
 	}else{
-		cout << "No solution exists" << endl;
-	}*/
-	return 0;
+		cout << "No solution exists";
+		return 0;
+	}
 }
+/*    int grid[N][N] = {{0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ 
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ 
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},
+ {0, 0, 0,   0, 0, 0,   0, 0, 0},};*/
